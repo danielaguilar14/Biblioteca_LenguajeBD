341e24418a592c58ef2e2b8d09da5d19c72519a9
danielaguilar14 <jaguilar30212@ufide.ac.cr>
2025-03-15 20:07:33 -0600

    Proyecto Biblioteca - Java. Jose Daniel Aguilar

---
+++ src/lenguajesbd_proyectofinal/LenguajesBD_ProyectoFinal.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lenguajesbd_proyectofinal;
+
+/**
+ *
+ * @author dagui
+ */
+public class LenguajesBD_ProyectoFinal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
